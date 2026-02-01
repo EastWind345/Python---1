7c9a4a8a1ed8b97627dd839ddd4b4f9501b80395
Alena <aki.6918@bk.ru>
2026-02-01 12:35:28 +0200

    Delete 03_lesson directory

--- 03_lesson/lesson_3_task_1.py
+++
@@ -1,9 +0,0 @@
-from user import User
-
-# Создаем экземпляр класса Product
-my_user = User("Nasta", "Last")
-
-# Вызываем методы и выводим результаты
-print(my_user.get_name())
-print(my_user.get_lname())
-print(my_user.get_name_lname())
