cbf2619f514be275c251fe018668786fe2376a1d
Alena <aki.6918@bk.ru>
2026-02-17 19:27:57 +0200

    Add files via upload

---
+++ 05_lesson/lesson05_task2.py
@@ -0,0 +1,25 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome(service=ChromeService
+                          (ChromeDriverManager().install()))
+driver.maximize_window()
+
+try:
+    driver.get(' http://uitestingplayground.com/dynamicid')
+
+    wait = WebDriverWait(driver, 10)
+    button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,
+                                                    'button.btn.btn-primary')))
+
+    button.click()
+    print("Кнопка нажата успешно.")
+
+finally:
+    driver.quit()
+sleep(10)
---
+++ 05_lesson/lesson05_task3.py
@@ -0,0 +1,34 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+
+driver = webdriver.Chrome(service=ChromeService
+                          (ChromeDriverManager().install()))
+
+
+try:
+    driver.get('http://the-internet.herokuapp.com/inputs')
+
+    wait = WebDriverWait(driver, 10)
+    input_field = wait.until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[type="number"]'))
+    )
+
+    input_field.send_keys('Sky')
+    print('Введено "Sky"')
+
+    input_field.clear()
+    print('Поле очищено')
+
+    input_field.send_keys('Pro')
+    print('Введено "Pro"')
+
+finally:
+    driver.quit()
+    print('Браузер закрыт')
+sleep(10)
