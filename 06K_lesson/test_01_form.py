12469179e9af4444f0a15d0b362884a4d2a80b9c
Alena <aki.6918@bk.ru>
2026-02-23 17:05:31 +0200

    Add files via upload

---
+++ 06K_lesson/test_01_form.py
@@ -0,0 +1,49 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+@pytest.fixture
+def browser():
+    driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
+
+
+def test_form_validation(browser):
+    browser.get("https://bonigarcia.dev/selenium-webdriver."
+                "-java/data-types.html")
+
+    fields = {
+        "first-name": "Иван",
+        "last-name": "Петров",
+        "address": "Ленина, 55-3",
+        "e-mail": "test@skypro.com",
+        "phone": "+7985899998787",
+        "zip-code": "",
+        "city": "Москва",
+        "country": "Россия",
+        "job-position": "QA",
+        "company": "SkyPro",
+    }
+    for field_name, value in fields.items():
+        browser.find_element(By.NAME, field_name).send_keys(value)
+
+    browser.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+
+    WebDriverWait(browser, 10).until(EC.visibility_of_element_located((
+        By.NAME, "zip-code")))
+
+    zip_code = browser.find_element(By.NAME, "zip-code")
+    assert "is-invalid" in zip_code.get_attribute("class")
+
+    valid_fields = [
+        "first-name", "last-name", "address", "e-mail",
+        "phone", "city", "country", "job-position", "company"
+    ]
+
+    for field_name in valid_fields:
+        field = browser.find_element(By.NAME, field_name)
+        assert "is-valid" in field.get_attribute("class")
---
+++ 06K_lesson/test_03_shop.py
@@ -0,0 +1,51 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+@pytest.fixture
+def browser():
+    driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
+
+
+def test_shopping_process(browser):
+
+    browser.get("https://www.saucedemo.com/")
+
+    user_name_field = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.ID, "user-name")))
+    user_name_field.send_keys("standard_user")
+    browser.find_element(By.ID, "password").send_keys("secret_sauce")
+    browser.find_element(By.ID, "login-button").click()
+
+    products_to_add = [
+        "Sauce Labs Backpack",
+        "Sauce Labs Bolt T-Shirt",
+        "Sauce Labs Onesie"
+    ]
+
+    for product_name in products_to_add:
+        add_button = browser.find_element(By.XPATH,
+                                          f"//div[contains(@class,"
+                                          f"'inventory_item') and .//div"
+                                          f"[contains(text(), '{product_name}'"
+                                          f"')]//button'")
+    add_button.click()
+
+    user_name_field(By.CLASS_NAME, "shopping_cart_link").click()
+    user_name_field(By.ID, "checkout").click()
+
+    browser.find_element(By.ID, "first-name").send_keys("Иван")
+    browser.find_element(By.ID, "last-name").send_keys("Иванов")
+    browser.find_element(By.ID, "postal-code").send_keys("123456")
+
+    browser.find_element(By.ID, "continue").click()
+
+    total_element = user_name_field(By.CLASS_NAME, "summary_total_label")
+    total_text = total_element.text
+
+    assert total_text == "Total: $58.29"
