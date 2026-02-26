4af89cd8db08309e908fb78363c3c3481c671de5
Alena <aki.6918@bk.ru>
2026-02-26 22:24:56 +0200

    Add files via upload

---
+++ 06K_lesson/test_02_calc.py
@@ -0,0 +1,31 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
+
+
+def test_slow_calculator(driver):
+    driver.get(
+        "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+
+    driver.find_element(By.CSS_SELECTOR, "#delay").clear()  # нашли элемент
+    driver.find_element(By.CSS_SELECTOR, "#delay").send_keys("45")
+
+    driver.find_element(By.XPATH, "//span[text()='7']").click()
+    driver.find_element(By.XPATH, "//span[text()='+']").click()
+    driver.find_element(By.XPATH, "//span[text()='8']").click()
+    driver.find_element(By.XPATH, "//span[text()='=']").click()
+
+    WebDriverWait(driver, 50).until(EC.text_to_be_present_in_element(
+        (By.CSS_SELECTOR, ".screen"), "15"))
+
+    result_text = driver.find_element(By.CSS_SELECTOR, ".screen").text
+    assert int(result_text) == 15
